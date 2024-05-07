5e90831a53c1666faa49bde96a70690c79f94e60
DanieleTrippodo <trippododaniele@gmail.com>
2024-05-07 13:27:59 +0200

    Finish Project

---
+++ script.js
@@ -0,0 +1,16 @@
+/* Chiediamo all'utente il suo Nome */
+const username = prompt("Inserisci il tuo Nome:");
+
+/* Chiediamo all'utente il suo Cognome */
+const surname = prompt("Inserisci il tuo Cognome:");
+
+/* Chiediamo all'utente il suo colore preferito */
+const FavoriteColor = prompt("Inserisci il tuo Colore Preferito:");
+
+
+
+
+
+
+/* Stampa il nome nella console */
+console.log(username, surname, FavoriteColor, 24);
